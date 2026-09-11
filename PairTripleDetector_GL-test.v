7db1e042833ed26cbaec084a7f6dcd4930fa5c4f
yw2835 <yw2835@cornell.edu>
2026-09-11 00:12:18 -0400

    Finished second test case in systematic testing with exhaustive testing, problem fixed

--- PairTripleDetector_GL-test.v
+++ PairTripleDetector_GL-test.v
@@ -89,7 +89,7 @@ module Top();
     // Add checks for exhaustive testing (check all possible inputs)
     //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     check( 0, 0, 0, 0 );
-    check( 0, 0, 1, 1 );
+    check( 0, 0, 1, 0 );
     check( 0, 1, 0, 0 );
     check( 0, 1, 1, 1 );
     check( 1, 0, 0, 0 );
